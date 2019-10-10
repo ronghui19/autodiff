c9b3a298402a19604d5e1c38eddecd018040488b
詹荣辉 <zhanronghui@liquidnetwork.com>
2019-10-10 15:16:23 +0800

    update file

--- autodiff.py
+++ autodiff.py
@@ -23,8 +23,8 @@ class Node:
 
 class Op:
     def __call__(self):
-        new_node = Node()
-        new_node.op = self
+        new_node = Node()  # create Node instance
+        new_node.op = self  # bind op with node
         return new_node
 
     def compute(self, node, input_vals):
@@ -38,16 +38,40 @@ class AddOp(Op):
         new_node = Op.__call__(self)
         new_node.inputs = [node_A, node_B]
         new_node.name = "(%s+%s)" % (node_A.name, node_B.name)
+        return new_node
+
+    def compute(self, node, input_vals):
+        assert len(input_vals) == 2
+        return input_vals[0] + input_vals[1]
+
+    def gradient(self, node, output_grad):
+        return [output_grad, output_grad]
+
+class AddByConstOp(Op):
+    def __call__(self, node_A, const_val):
+        new_node = Op.__call__(self)
+        new_node.inputs = [node_A]
+        new_node.const_attr = const_val
+        new_node.name = "(%s+%s)" % (node_A.name, str(const_val))
+        return new_node
+
+    def compute(self, node, input_vals):
+        assert len(input_vals) == 2
+        return input_vals[0] + node.const_attr
+
+    def gradient(self, node, out_grad):
+        return [out_grad]
+
+class MulOp(Op):
+    def __call__(self, node_A, node_B):
+        new_node = Op.__call__(self)
+        new_node.inputs = [node_A, node_B]
+        new_node.name = "(%s*%s)" % (node_A.name, node_B.name)
+        return new_node
+
+    def compute(self, node, input_vals):
 
 
-
-class AddByConstOp:
-    def __call__(self, *args, **kwargs):
-        pass
-
-class MulOp:
-    def __call__(self, *args, **kwargs):
-        pass
 
 class MulByConstOp:
     def __call__(self, *args, **kwargs):
